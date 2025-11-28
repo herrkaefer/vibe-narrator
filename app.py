c5cd6b6c85282d6e0ad9faa44f27670a0a35195d
herrkaefer <gloolar@gmail.com>
2025-11-28 11:08:40 -0600

    Add logo and update app to display it

--- app.py
+++ app.py
@@ -3,5 +3,9 @@ import gradio as gr
 def greet(name):
     return "Hello " + name + "!!"
 
-demo = gr.Interface(fn=greet, inputs="text", outputs="text")
+# Create interface with logo
+with gr.Blocks(title="Vibe Narrator") as demo:
+    gr.Image("logo.png", show_label=False, container=False, height=120)
+    gr.Interface(fn=greet, inputs="text", outputs="text")
+
 demo.launch()
